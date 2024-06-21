58d22d92b57e12cbad85711fc0a36f9d3b5a26fe
Pushpitha <pushpithagee@gmail.com>
2024-06-21 17:09:08 +0530

    fix:Hero-section mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ export default function Home() {
        <div className="h-[2px] w-full bg-main-gradient absolute top-0 z-50"></div>
       <Navbar></Navbar>
       <section className="w-full h-screen">
-        <div className="h-[98%] relative bg-[url('/assets/bg-hero.png')] bg:contain md:bg-cover">
+        <div className="h-[85%] md:h-[98%] relative bg-[url('/assets/bg-hero.png')] bg:contain md:bg-cover">
           <div className="h-[85px]"></div>
           {/* <div className="h-[2px] w-full bg-main-gradient absolute top-0"></div> */}
           <div className="h-[2px] w-full bg-main-gradient absolute bottom-0"></div>
           <img className="absolute right-0 bottom-[2px] h-[80%] hidden md:block" src="/assets/avatar.png"></img>
           {/* <Button className="px-6 py-0 text-xs font-black rounded uppercase bg-primary/50 border border-primary-foreground">Event Schedule</Button> */}
-          <div className="container mx-auto h-[calc(98%-150px)] ">
-            <div className="md:pt-32 pt-24 md:pl-24">
+          <div className="container mx-auto md:h-[calc(98%-150px)] ">
+            <div className="md:pt-32 pt-12 md:pl-24">
               <div className="mb-6 md:mb-3 font-semibold">IEEE Sri Lanka Section Student | YP | WIE CONGRESS </div>
               <div className="font-bold text-[4rem] leading-[4rem] md:text-[6rem] md:leading-[6rem] ">
                 <div className="flex-col inline-flex bg-text-gradient text-transparent bg-clip-text">
